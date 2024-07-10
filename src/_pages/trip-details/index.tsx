c059561aae50e1d274fe6475cacc50c88d840654
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-09 21:05:54 -0300

    refactor: separate trip details page into useful components

--- src/_pages/trip-details/index.tsx
+++ src/_pages/trip-details/index.tsx
@@ -1,15 +1,10 @@
-import {
-    Calendar,
-    CircleCheck,
-    CircleDashed,
-    Link2,
-    MapPin,
-    Plus,
-    Settings2,
-    UserCog,
-} from 'lucide-react';
+import { Plus } from 'lucide-react';
 import { useState } from 'react';
 import CreateActivityModal from './_components/create-activity-modal';
+import ImportantLinks from './_components/important-links';
+import Guests from './_components/guests';
+import Activities from './_components/activities';
+import DestinationAndDateHeader from './_components/destination-and-date-header';
 
 const TripDetailsPage = () => {
     const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false);
@@ -19,24 +14,7 @@ const TripDetailsPage = () => {
     };
     return (
         <div className="mx-auto max-w-6xl space-y-8 px-6 py-10">
-            <div className="flex h-16 items-center justify-between rounded-xl bg-zinc-900 px-4 shadow-shape">
-                <div className="flex items-center gap-2">
-                    <MapPin className="size-5 text-zinc-400" />
-                    <span className="ext-zinc-100">Florianópolis, Brasil</span>
-                </div>
-
-                <div className="flex items-center gap-5">
-                    <div className="flex items-center gap-2">
-                        <Calendar className="size-5 text-zinc-400" />
-                        <span className="text-zinc-100">16 a 27 de Agosto</span>
-                    </div>
-                    <div className="h-6 w-px bg-zinc-800"></div>
-                    <button className="flex items-center gap-2 rounded-lg bg-zinc-800 px-5 py-2 font-medium text-zinc-200 hover:bg-zinc-700">
-                        Alterar local/data
-                        <Settings2 className="size-5" />
-                    </button>
-                </div>
-            </div>
+            <DestinationAndDateHeader />
 
             <main className="flex gap-16 px-4">
                 <div className="flex-1 space-y-6">
@@ -52,112 +30,11 @@ const TripDetailsPage = () => {
                         </button>
                     </div>
 
-                    <div className="space-y-8">
-                        <div className="space-y-2.5">
-                            <div className="flex items-baseline gap-2">
-                                <span className="text-xl font-semibold text-zinc-300">Dia 17</span>
-                                <span className="text-xs text-zinc-500">Sábado</span>
-                            </div>
-                            <p className="text-sm text-zinc-500">
-                                Nenhuma atividade cadastrada nessa data.
-                            </p>
-                        </div>
-
-                        <div className="space-y-2.5">
-                            <div className="flex items-baseline gap-2">
-                                <span className="text-xl font-semibold text-zinc-300">Dia 18</span>
-                                <span className="text-xs text-zinc-500">Domingo</span>
-                            </div>
-                            <div className="space-y-2.5">
-                                <div className="flex items-center gap-3 rounded-xl bg-zinc-900 px-4 py-2.5 shadow-shape">
-                                    <CircleCheck className="size-5 text-lime-300" />
-                                    <span className="text-zinc-100">Academia em grupo</span>
-                                    <span className="ml-auto text-sm text-zinc-400">08:00h</span>
-                                </div>
-                            </div>
-                            <div className="space-y-2.5">
-                                <div className="flex items-center gap-3 rounded-xl bg-zinc-900 px-4 py-2.5 shadow-shape">
-                                    <CircleCheck className="size-5 text-lime-300" />
-                                    <span className="text-zinc-100">Academia em grupo</span>
-                                    <span className="ml-auto text-sm text-zinc-400">08:00h</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Activities />
                 </div>
                 <div className="w-80 space-y-6">
-                    <div className="space-y-6">
-                        <h2 className="text-xl font-semibold">Links importantes</h2>
-                        <div className="space-y-5">
-                            <div className="flex items-center justify-between gap-4">
-                                <div className="space-y-1.5">
-                                    <span className="block font-medium text-zinc-100">
-                                        Reserva do AirBnB
-                                    </span>
-                                    <a
-                                        href="#"
-                                        className="block truncate text-xs text-zinc-400 hover:text-zinc-200"
-                                    >
-                                        https://www.airbnb.com.br/rooms/104700011
-                                    </a>
-                                </div>
-                                <Link2 className="size-5 shrink-0 text-zinc-400" />
-                            </div>
-
-                            <div className="flex items-center justify-between gap-4">
-                                <div className="space-y-1.5">
-                                    <span className="block font-medium text-zinc-100">
-                                        Reserva do AirBnB
-                                    </span>
-                                    <a
-                                        href="#"
-                                        className="block truncate text-xs text-zinc-400 hover:text-zinc-200"
-                                    >
-                                        https://www.airbnb.com.br/rooms/104700011
-                                    </a>
-                                </div>
-                                <Link2 className="size-5 shrink-0 text-zinc-400" />
-                            </div>
-                        </div>
-                        <button className="flex h-11 w-full items-center justify-center gap-2 rounded-lg bg-zinc-800 px-5 font-medium text-zinc-200 hover:bg-zinc-700">
-                            <Plus className="size-5" />
-                            Cadastrar novo link
-                        </button>
-                    </div>
-                    <div className="h-px w-full bg-zinc-800">
-                        <div className="space-y-6">
-                            <h2 className="text-xl font-semibold">Convidados</h2>
-                            <div className="space-y-5">
-                                <div className="flex items-center justify-between gap-4">
-                                    <div className="space-y-1.5">
-                                        <span className="block font-medium text-zinc-100">
-                                            Jessica White
-                                        </span>
-                                        <span className="block truncate text-sm text-zinc-400">
-                                            jessica.white44@yahoo.com
-                                        </span>
-                                    </div>
-                                    <CircleDashed className="size-5 shrink-0 text-zinc-400" />
-                                </div>
-
-                                <div className="flex items-center justify-between gap-4">
-                                    <div className="space-y-1.5">
-                                        <span className="block font-medium text-zinc-100">
-                                            Gabriel Feijó
-                                        </span>
-                                        <span className="block truncate text-sm text-zinc-400">
-                                            lacy.stiedemann@gmail.com
-                                        </span>
-                                    </div>
-                                    <CircleDashed className="size-5 shrink-0 text-zinc-400" />
-                                </div>
-                            </div>
-                            <button className="flex h-11 w-full items-center justify-center gap-2 rounded-lg bg-zinc-800 px-5 font-medium text-zinc-200 hover:bg-zinc-700">
-                                <UserCog className="size-5" />
-                                Gerenciar convidados
-                            </button>
-                        </div>
-                    </div>
+                    <ImportantLinks />
+                    <Guests />
                 </div>
             </main>
 
